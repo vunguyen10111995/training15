4c575130eb7bde50ffcb0c9776b8ebcae459967b
Bùi Anh Tuấn <anhtuan196.kn@gmail.com>
2019-03-04 17:22:35 +0700

    migration

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,35 @@ Route::get('Controller','MyController@xinchao');
 Route::get('View','MyController@myView');
 
 
-Route::get('{ten}',function($ten){
-	echo $ten." Bạn Thật Đẹp Trai";
+// Route::get('{ten}',function($ten){
+// 	echo $ten." Bạn Thật Đẹp Trai";
+// });
+
+
+Route::get('database',function(){
+	// Schema::create('loaisanpham',function($table){
+	// 	$table->increments('id');
+	// 	$table->string('ten',200);
+	// });
+	// echo "Tạo Bảng Thành Công";
+
+	Schema::create('theloai',function($table){
+		$table->increments('id');
+		$table->string('ten',200)->nullable();
+		$table->string('nsx')->default('Nha San Xuat');
+	
+	});
+	echo "Lại Tạo Bảng Thành Công";
+});
+
+Route::get('lienketbang',function(){
+	Schema::create('sanpham',function($table){
+		$table->increments('id');
+		$table->string('ten');
+		$table->float('gia');
+		$table->integer('soluong')->default(0);
+		$table->integer('id_loaisanpham')->unsigned();
+		$table->foreign('id_loaisanpham')->references('id')->on('loaisanpham');
+	});
+	echo "Tiếp Tục Tạo Thành Công";
 });
